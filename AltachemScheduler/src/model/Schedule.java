6661accd4f75e01eeffa838bace710f6707ec09d
Christophe De Backer <christophe.db@live.be>
2020-11-09 12:02:10 +0100

    merge tool fix

--- AltachemScheduler/src/model/Schedule.java
+++ AltachemScheduler/src/model/Schedule.java
@@ -1,14 +1,10 @@
 package model;
 
 public class Schedule {
-<<<<<<< HEAD
 	private Machine[] machines;
 	private Item[] items;
 	
 	public int amountOfMachines() {
 		return machines.length;
 	}
-=======
- //test push
->>>>>>> branch 'master' of https://github.com/CrimsonKobalt/AltachemScheduler.git
 }
