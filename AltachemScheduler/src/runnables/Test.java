52c0bbb84f833368fbabb7bef7c44db9f06a8c12
BenteVE <44496102+BenteVE@users.noreply.github.com>
2020-11-23 17:33:26 +0100

    run validator from java

--- AltachemScheduler/src/runnables/Test.java
+++ AltachemScheduler/src/runnables/Test.java
@@ -1,6 +1,5 @@
 package runnables;
 
-import examples.Reader;
 import model.Problem;
 import solution.Idle;
 import solution.Job;
@@ -8,6 +7,7 @@ import solution.Maintenance;
 import solution.Production;
 import solution.Solution;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,10 +29,39 @@ public class Test {
         filenames.add("src/examples/A_40_100_60.txt");
 
         for (String filename : filenames){
+            String outputFilename = filename.substring(0, filename.length()-4) + "_sol.txt";
             Problem problem = new Problem(filename);
             Solution solution = Solution.CreateInitialSolution(problem);
-            solution.write(filename.substring(0, filename.length()-4) + "_sol.txt");
+            solution.write(outputFilename);
+
+            // check with validator
+            try{
+                    Process proc = Runtime.getRuntime().exec("java -jar src/examples/AspValidator.jar -i " + filename + " -s " + outputFilename + " -l src/examples/validatorOutput.txt");
+
+                    System.out.println("Validator Output for file: " + filename);
+
+                    // Read the output from the command
+                    BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                    String s = null;
+                    while ((s = stdInput.readLine()) != null) {
+                            System.out.println(s);
+                    }
+
+                    // Read any errors from the attempted command
+                    BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+                    while ((s = stdError.readLine()) != null) {
+                            System.out.println(s);
+                    }
+
+                    System.out.println();
+
+            }catch (IOException ioException){
+                    System.out.println("An error occurred.");
+                    ioException.printStackTrace();
+            }
+
         }
+
 	}
 
 }
